f8aceae49f890237753198a6b838a1ac1db0ac21
JojiKoike <joji.koike@gmail.com>
2020-09-20 15:00:56 +0900

    :+1: SecureSession対応

---
+++ app/services/session/SessionService.scala
@@ -0,0 +1,47 @@
+package services.session
+
+import java.math.BigInteger
+import java.security.SecureRandom
+
+import javax.inject.{Inject, Singleton}
+import akka.actor.typed.{ActorRef, Scheduler}
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.util.Timeout
+import services.session.SessionCache._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class SessionService @Inject() (cacheActor: ActorRef[Command])(implicit ec: ExecutionContext, scheduler: Scheduler) {
+  implicit private val timeout: Timeout = akka.util.Timeout(300.milliseconds)
+
+  def create(secretKey: Array[Byte]): Future[String] = {
+    val sessionId = newSessionId()
+    cacheActor ! PutInCache(sessionId, secretKey)
+    Future.successful(sessionId)
+  }
+
+  def lookup(sessionId: String): Future[Option[Array[Byte]]] = {
+    (cacheActor ? (GetFromCache(sessionId, _))).map {
+      case Cached(key: Any, value: Option[_]) =>
+        value.asInstanceOf[Option[Array[Byte]]]
+    }
+  }
+
+  def put(sessionId: String, secretKey: Array[Byte]): Future[Unit] = {
+    cacheActor ! PutInCache(sessionId, secretKey)
+    Future.successful(())
+  }
+
+  def delete(sessionId: String): Future[Unit] = {
+    cacheActor ! Evict(sessionId)
+    Future.successful(())
+  }
+
+  private val sr = new SecureRandom()
+
+  private def newSessionId(): String = {
+    new BigInteger(130, sr).toString(32)
+  }
+}
